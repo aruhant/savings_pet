62d2c2d1f5f167c41fdd92d4df1a14f006dd0f12
Aruhant Mehta <125254014+aruhant@users.noreply.github.com>
2025-09-13 11:56:25 -0700

    fixed macos entitlements

--- RoyalPig/test/widget_test.dart
+++ RoyalPig/test/widget_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:figma_to_flutter_app/main.dart'; // update with your package name
+import 'package:goals/main.dart';
 
 void main() {
   testWidgets('App builds and shows Home', (WidgetTester tester) async {
